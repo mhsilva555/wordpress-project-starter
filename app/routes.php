b2e2f5d615cdb544735fba7fd1dee5034d9a3bbf
Marcos Henrique <mhsilva555@gmail.com>
2022-03-20 18:00:36 -0300

    Initial Commit

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-Routes::map('clientes-cadastrados/detalhes-cliente/:id', function($params){
-    Routes::load(VIEWS . 'partials/cadastros/detalhes-cliente.blade.php', $params);
-});
-
-Routes::map('/clientes-cadastrados/search', function () {
-    $params = $_GET;
-    $results = \App\Controllers\ListarClientes::searchClients($params);
-    Routes::load(VIEWS . 'page-clientes-cadastrados.blade.php', $results);
+Routes::map('ENDPOINT', function(){
+    Routes::load(VIEWS . 'BLADE_TEMPLATE', null, null, 200);
 });
